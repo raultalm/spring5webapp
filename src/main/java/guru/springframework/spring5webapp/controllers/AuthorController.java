6a0bfbccf96c8b1e691112bea03c48fb6e94742f
raultalm <raultalmacel@yahoo.com>
2021-10-06 11:42:03 +0200

    Statment 3
    Create author controller + display a table of authors

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll()); // this will be iterated inside the authors/list.html
+        return "authors/list"; // Return the identifier for the view
+    }
+}
